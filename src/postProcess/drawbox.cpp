90c2079ee03057874cdf8b072b166202d7c4bc6e
Jae Hoon (Daniel) Lee <danieljh.lee@lge.com>
2024-05-24 16:33:12 +0900

    Fix bug in drawbox
    :Release Notes:
    Fix bug in drawbox
    
    :Detailed Notes:
    Fix bug in drawbox
    
    :Testing Performed:
    OK
    
    :QA Notes:
    N/A
    
    :Issues Addressed:
    [WRQ-22394] Fix bug in drawbox
    
    Change-Id: Ia921bc0fbdaea65cb7690d595621407531f79c64

--- src/postProcess/drawbox.cpp
+++ src/postProcess/drawbox.cpp
@@ -100,7 +100,7 @@ void drawBoxInNV12Format(uint8_t *data, const int &maxWidth,
     uint16_t *tuple = reinterpret_cast<uint16_t *>(data + offsetUV);
 
     uint16_t x1 = face.x, x2 = (USHRT_MAX - face.x < face.w ? 0 : face.x + face.w);
-    uint16_t y1 = face.y, y2 = (USHRT_MAX - face.y < face.h ? 0 : face.x + face.w);
+    uint16_t y1 = face.y, y2 = (USHRT_MAX - face.y < face.h ? 0 : face.y + face.h);
 
     x1 = (x1 > maxWidth) ? maxWidth : x1;
     x2 = (x2 > maxWidth) ? maxWidth : x2;
